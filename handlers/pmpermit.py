700b2e0ee65d858cd75567bc77ec1dcb2136f12a
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-04-27 21:28:25 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -24,5 +24,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service of @groupvcRobot .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n  ⚠️ Note : if you are unable to add music assistant in  private groups just make the group public for one minute and send the group link here or in @God_loverr dm and after the music assistant joins you can make your group private again\n    ❗if you need any help just ask here @CheemsUserbot\n\n")
+  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service of @MusicVcRobot .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n  ⚠️ Note : if you are unable to add music assistant in  private groups just make the group public for one minute and send the group link here or in @God_loverr dm and after the music assistant joins you can make your group private again\n    ❗if you need any help just ask here @CheemsUserbot\n\n")
   return                        
