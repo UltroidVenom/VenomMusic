319686eeff1ba0b36fba1c60efd3cfcb23fed89e
UltroidVenom <80195199+UltroidVenom@users.noreply.github.com>
2021-05-06 18:10:54 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -6,5 +6,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hi there, This is the music assistant service of @musicvc10bot .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK HERE OF PUBLIC GROUPS IF YOU CANT ADD ASSISTANT** \n\n **⚠️ Note : if you are unable to add music assistant in  private groups just make the group public for one minute and send the group link here or in @God_loverr dm and after the music assistant joins you can make your group private again** \n\n **❗if you need any help just ask here @CheemsUserbot**")
+  await USER.send_message(message.chat.id,"Hi there, This is the music assistant service of @VenomMusicBot .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **IF YOU HAVE ANY PROBLEMS REGARDING MUSIC BOT KINDLY CONTACT @R2K_VENOM FOR QUERIES** \n\n **⚠️ Note : if you are unable to add music assistant in  private groups just make the group public for one minute and send the group link here or in @R2K_VENOM dm and after the music assistant joins you can make your group private again** \n\n **❗if you need any help just ask here @CrackMonkey**")
   return                        
